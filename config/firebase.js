7f2df70cecda454e65296390eae8271b20a9f600
Jose Andreas <jose_andrea@icloud.com>
2026-04-14 15:33:43 +0700

    chore: change firebase to firebase-admin

--- config/firebase.js
+++ config/firebase.js
@@ -1,21 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const admin = require("firebase-admin");
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDu7RGCk-k4LOgPARrlQB2SnQ8xLqtEsJk",
-  authDomain: "levelio-5d169.firebaseapp.com",
-  projectId: "levelio-5d169",
-  storageBucket: "levelio-5d169.firebasestorage.app",
-  messagingSenderId: "542161805570",
-  appId: "1:542161805570:web:c25493cd04e58a1128a21b",
-  measurementId: "G-3RSGXPTCSX"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.FB_CLIENT_EMAIL,
+    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+});
