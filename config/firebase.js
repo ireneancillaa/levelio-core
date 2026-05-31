7aee390df09a22bbc836df7dfd4ad27eebd65cc5
Jose Andreas <jose_andreas@icloud.com>
2026-05-31 12:41:32 +0700

    fix: firebase private key format

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,19 @@
 const admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FB_PROJECT_ID,
-    clientEmail: process.env.FB_CLIENT_EMAIL,
-    privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
-});
+const privateKey = process.env.FB_PRIVATE_KEY
+  ? process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n")
+  : undefined;
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FB_PROJECT_ID,
+      clientEmail: process.env.FB_CLIENT_EMAIL,
+      privateKey: privateKey,
+    }),
+  });
+  console.info("[INFO] Firebase Admin sukses diinisialisasi");
+}
 
 const auth = admin.auth();
 const db = admin.firestore();
