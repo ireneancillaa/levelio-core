6178998ee69951b13b4ff68e98f76b9a0bba15b1
Jose Andreas <jose_andreas@icloud.com>
2026-05-31 13:02:35 +0700

    fix: firebase base64

--- config/firebase.js
+++ config/firebase.js
@@ -1,18 +1,27 @@
 const admin = require("firebase-admin");
 
-const privateKey = process.env.FB_PRIVATE_KEY
-  ? Buffer.from(process.env.FB_PRIVATE_KEY, "base64").toString("utf8")
-  : undefined;
+let serviceAccount;
+
+if (process.env.FB_SERVICE_ACCOUNT_BASE64) {
+  const decryptedJson = Buffer.from(process.env.FB_SERVICE_ACCOUNT_BASE64, "base64").toString("utf8");
+  serviceAccount = JSON.parse(decryptedJson);
+}
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FB_PROJECT_ID,
-      clientEmail: process.env.FB_CLIENT_EMAIL,
-      privateKey: privateKey,
-    }),
-  });
-  console.info("[INFO] Firebase Admin sukses diinisialisasi via Base64");
+  if (serviceAccount) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
+    });
+    console.info("[INFO] Firebase Admin sukses diinisialisasi via Full JSON Base64");
+  } else {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FB_PROJECT_ID,
+        clientEmail: process.env.FB_CLIENT_EMAIL,
+        privateKey: process.env.FB_PRIVATE_KEY ? process.env.FB_PRIVATE_KEY.replace(/\\n/g, "\n") : undefined,
+      }),
+    });
+  }
 }
 
 const auth = admin.auth();
