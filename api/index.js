2d4469a72d0465b4ebe032a2bff55ed1952e25ef
Jose Andreas <jose_andreas@icloud.com>
2026-05-31 12:22:39 +0700

    chore: add vercel config

--- api/index.js
+++ api/index.js
@@ -1,28 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// routes
-const accountRoutes = require("../features/account");
-const habitRoutes = require("../features/habit");
-
-// prefix
-app.use("/account", accountRoutes);
-app.use("/habit", habitRoutes);
-
-app.get("/", (req, res) => {
-  res.json({ status: "online", message: "levelio API is online" });
-});
-
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+const app = require("../app");
 
 module.exports = app;
---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// rutes (Path disesuaikan karena app.js ada di luar folder api)
+const accountRoutes = require("./features/account");
+const habitRoutes = require("./features/habit");
+
+// prefix
+app.use("/account", accountRoutes);
+app.use("/habit", habitRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ status: "online", message: "levelio API is online" });
+});
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
